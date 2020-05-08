c24496dba1ae21aee5173dd9aa447b4ca669d25f
devnikop <petrov.petrov.ak@gmail.com>
2020-05-08 10:49:07 +0300

    update test for video-player

--- src/components/movie-cards/__tests__/movie-cards.e2e.test.jsx
+++ src/components/movie-cards/__tests__/movie-cards.e2e.test.jsx
@@ -7,18 +7,21 @@ const mock = {
   movies: [
     {
       id: 1,
+      poster: `image`,
       src: `source_11`,
       title: `Fantastic Beasts`,
     },
     {
       id: 2,
+      poster: `image2`,
       src: `source_22`,
       title: `Aviator`,
     },
     {
       id: 3,
+      poster: `image`,
       src: `source_33`,
-      title: `Bohemian rhapsody`,
+      title: `Drive`,
     },
   ],
 };
@@ -26,26 +29,18 @@ const mock = {
 describe(`mouse events`, () => {
   const { movies } = mock;
 
-  it(`mouseEnter&mouseLeave on movieCard change state idActive`, () => {
+  it(`mouseEnter&mouseLeave on movieCard change state idPlay`, () => {
+    jest.useFakeTimers();
     const firstMovieId = movies[0].id;
     const component = mount(<MovieCards movies={movies} />);
     const movieCard = component.find(`.small-movie-card`);
 
-    expect(component.state(`idActive`)).toBe(-1);
+    expect(component.state(`idPlay`)).toBe(-1);
     movieCard.at(0).simulate(`mouseEnter`);
-    expect(component.state(`idActive`)).toBe(firstMovieId);
+    jest.advanceTimersByTime(1000);
+    expect(component.state(`idPlay`)).toBe(firstMovieId);
 
     movieCard.at(0).simulate(`mouseLeave`);
-    expect(component.state(`idActive`)).toBe(-1);
-  });
-
-  it(`click on play button change state idPlay`, () => {
-    const firstMovieId = movies[0].id;
-    const component = mount(<MovieCards movies={movies} />);
-    const playButton = component.find(`.small-movie-card__play-btn`);
-
     expect(component.state(`idPlay`)).toBe(-1);
-    playButton.at(0).simulate(`click`);
-    expect(component.state(`idPlay`)).toBe(firstMovieId);
   });
 });
